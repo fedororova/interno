dbc515dbc0db18ece6d13ac3300ccca5fcc7d78f
fedororova <al.fedorova02@gmail.com>
2023-05-03 22:35:47 +0700

    Панель поиска, блоки главной страницы

---
+++ script/script.js
@@ -0,0 +1,8 @@
+//search
+
+function openModal() {
+    document.getElementById("overlay").style.top = "0px";
+}
+function closeModal() {
+    document.getElementById("overlay").style.top = "-100vh";
+}
