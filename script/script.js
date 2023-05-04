8459ca98ff9f9a6d8f7c28b6e84b56159a5096df
fedororova <al.fedorova02@gmail.com>
2023-05-04 20:41:58 +0700

    Блоки главной страницы

--- script/script.js
+++ script/script.js
@@ -3,6 +3,20 @@
 function openModal() {
     document.getElementById("overlay").style.top = "0px";
 }
+
 function closeModal() {
     document.getElementById("overlay").style.top = "-100vh";
 }
+
+/*Swiper*/
+var swiper = new Swiper(".swiper-feedback", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 30,
+        slideShadows: false,
+    },
+});
